b65b21bfad756e2aab84aeb444460e925b7c17c1
Bhora Aaltonen <Bhora.Aaltonen@student.laurea.fi>
2026-09-16 17:55:45 +0300

    js ja css

--- WS02_JavaScript_Basics/js/script.js
+++ WS02_JavaScript_Basics/js/script.js
@@ -1,82 +1,38 @@
-// STEP 1: Print text to the browser console is string ""
-
-<<<<<<< HEAD
+// STEP 1: Print text to the browser console
 console.log("Hello world!");
-=======
-console.log("Hello, World!");
->>>>>>> fd67f03f2e5ecf6a334b5d91e4e12040df73af7e
 
 // STEP 2: Variables
-
 // Store text values in variables
-
-<<<<<<< HEAD
 const userName = "Bhora";
 const favoriteAnimal = "Cat";
 
-
 // Print the variables to the console
-
 console.log("User Name: " + userName);
-console.log("Favorite Animal " + favoriteAnimal );
+console.log("Favorite Animal: " + favoriteAnimal);
 
 // STEP 3: User interaction
 // Display a pop-up message
-
-alert("Tervettuloa minun hienolle nettisivulle!");
-
-=======
-const userName = "Juho";
-const favoriteAnimal = "Blue Elephant";
-
+alert("Tervetuloa minun hienolle nettisivulleni!");
 
 // Print the variables to the console
 console.log("My name is " + userName);
 console.log("My favorite animal is " + favoriteAnimal);
-// STEP 3: User interaction
-// Display a pop-up message
-alert("tervertetuloa hieoulle nettisivylleni!");
->>>>>>> fd67f03f2e5ecf6a334b5d91e4e12040df73af7e
 
 // Ask the user for their name
-
 const visitorName = prompt("Mikä on nimesi?");
-<<<<<<< HEAD
-// Print the user's answer
-
-console.log(visitorName);
-
-console.log("Terve " + visitorName + "! Tervetuloa javascriptin pariin!");
-
-
-// Ask the user for their favorite animal
-const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?")
-=======
 
 // Print the user's answer
-console.log(visitorName);
+console.log("Terve " + visitorName + "! Tervetuloa JavaScriptin pariin!");
 
-console.log("Terve " + visitorName + "! Tervetuloa javasciptimn psariin!");
 // Ask the user for their favorite animal
-
 const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
->>>>>>> fd67f03f2e5ecf6a334b5d91e4e12040df73af7e
 
 // Create a sentence using both answers
-
 console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
 
-// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
-// ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
-
-<<<<<<< HEAD
+// ⭐⭐ BONUS: If / Else statement
 if (visitorFavoriteAnimal.toLowerCase() === "blue elephant") {
     console.log("Great choice! That's my favorite animal too!");
 } else {
     console.log("Nice! Your favorite animal is " + visitorFavoriteAnimal + ".");
 }
-
-=======
-// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
->>>>>>> fd67f03f2e5ecf6a334b5d91e4e12040df73af7e
-
